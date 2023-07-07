fae1945d8eecd7f481a059d10b602edf78d9e105
CodeScript990 <trte135@naver.com>
2023-07-07 21:38:15 +0900

    feat.July.7.23 SpringBoot - [Security(JWT)]  - Content :     1. SpringBoot Security JWT 의존성 추가와 Cookie 에 대해서 학습하고 어떻게 동작하는지와 Security 에서 제공하는 애노테이션을 이용하여 HTTP 통신을 주고받는 실습을 함

--- src/main/java/com/example/auth/config/WebSecurityConfig.java
+++ src/main/java/com/example/auth/config/WebSecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -19,11 +20,14 @@ import org.springframework.security.web.SecurityFilterChain;
 public class WebSecurityConfig {
     // 설정으로 등록 작업
     // @Bean : 메서드의 결과를 BEAN 객체로 등록해주는 어노테이션을 말함
-    @Bean // 설명 : return 을 해주는 결과가 빈 어노테이션에게 부여하여 spring container 에 의해 관리를 해준다는 의미임
+    @Bean  // 메소드의 결과를 Bean 객체로 등록해주는 어노테이션
     public SecurityFilterChain securityFilterChain(
-            HttpSecurity http // DI 자동으로 설정됨, 빌더 패턴 처럼 쓴다는 의미
-    ) throws Exception {
+            // DI 자동으로 설정됨, 빌더 패턴 처럼 쓴다.
+            HttpSecurity http
+    )
+            throws Exception {
         http
+                // CSRF: Cross Site Request Forgery
                 .csrf(AbstractHttpConfigurer::disable)
                 // 1. requestMatchers를 통해 설정할 URL 지정
                 // 2. permitAll(), authenticated() 등을 통해 어떤 사용자가
@@ -32,63 +36,77 @@ public class WebSecurityConfig {
                         authHttp -> authHttp // HTTP 요청 허가 관련 설정을 하고 싶다.
                                 // requestMatchers == 어떤 URL로 오는 요청에 대하여 설정하는지
                                 // permitAll() == 누가 요청해도 허가한다.
-                                .requestMatchers("/no-auth")
+                                .requestMatchers(
+                                        "/no-auth",
+                                        "/token/issue"
+                                )
                                 .permitAll()
                                 .requestMatchers(
                                         "/re-auth",
                                         "/users/my-profile"
                                 )
                                 .authenticated()  // 인증이 된 사용자만 허가
-                                .requestMatchers("/", "/users/register")
+                                .requestMatchers(
+                                        "/",
+                                        "/users/register"
+                                )
                                 .anonymous()  // 인증이 되지 않은 사용자만 허가
                 )
-                // form 을 이용한 로그인 관련 설정
-                .formLogin(
-                        formLogin -> formLogin
-                                // 로그인 하는 페이지(경로)를 지정 => login
-                                .loginPage("/users/login")
-                                // 로그인 성공시 이동하는 페이지(경로) => my-profile
-                                .defaultSuccessUrl("/users/my-profile")
-                                // 로그인 실패시 이동하는 페이지(경로) => fail
-                                .failureUrl("/users/login?fail")
-                                // 로그인 과정에서 필요한 경로들을
-                                // 모든 사용자가 사용할 수 있게끔 권한설정
-                                .permitAll()
-                )
-                // 로그아웃 관련 설정
-                // 로그인 -> 쿠키를 통해 세션을 생성
-                //   필수 : (아이디와 비밀번호)
-                // 로그아웃 -> 세션을 제거
-                //         -> 세션정보만 있으면 제거 가능함
-                .logout(logout -> logout
-                        // 로그아웃 요청을 보낼 URL
-                        // 어떤 UI에 로그아웃 기능을 연결하고 싶으면 해당 UI가 /users/logout 으로 POST 요청을 보내게끔
-                        .logoutUrl("/users/logout")
-                        // 로그아웃 성공시 이동할 URL 설정 => login
-                        .logoutSuccessUrl("/users/login")
+                .sessionManagement(
+                        sessionManagement -> sessionManagement
+                                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 );
-        // 401 => 인증되지 않음 ,403 => 누군지는 알지만 허가되지 않음
+//                // form 을 이용한 로그인 관련 설정
+//                .formLogin(
+//                        formLogin -> formLogin
+//                                // 로그인 하는 페이지(경로)를 지정
+//                                .loginPage("/users/login")
+//                                // 로그인 성공시 이동하는 페이지(경로)
+//                                .defaultSuccessUrl("/users/my-profile")
+//                                // 로그인 실패시 이동하는 페이지(경로)
+//                                .failureUrl("/users/login?fail")
+//                                // 로그인 과정에서 필요한 경로들을
+//                                // 모든 사용자가 사용할 수 있게끔 권한설정
+//                                .permitAll()
+//                )
+//                // 로그아웃 관련 설정
+//                // 로그인 -> 쿠키를 통해 세션을 생성
+//                //       아이디와 비밀번호
+//                // 로그아웃 -> 세션을 제거
+//                //        -> 세션 정보만 있으면 제거 가능
+//                .logout(
+//                        logout -> logout
+//                                // 로그아웃 요청을 보낼 URL
+//                                // 어떤 UI에 로그아웃 기능을 연결하고 싶으면
+//                                // 해당 UI가 /users/logout으로 POST 요청을
+//                                // 보내게끔
+//                                .logoutUrl("/users/logout")
+//                                // 로그아웃 성공시 이동할 URL 설정
+//                                .logoutSuccessUrl("/users/login")
+//                );
+
         return http.build();
     }
 
-    @Bean
-    // 사용자 관리를 위한 Interface 구현체 Bean
+    //    @Bean
+    // 사용자 관리를 위한 인터페이스 구현체 Bean
     public UserDetailsManager userDetailsManager(
-            PasswordEncoder passwordEncoder // Password 인증을 활용하기 위한 작업
+            PasswordEncoder passwordEncoder
     ) {
         // 임시 User
-        UserDetails user1 = User.withUsername("test")
-                .password(passwordEncoder.encode("test1234"))
+        UserDetails user1 = User.withUsername("user1")
+                .password(passwordEncoder.encode("password"))
                 .build();
         // Spring 에서 미리 만들어놓은 사용자 인증 서비스
-        return new InMemoryUserDetailsManager(user1); // 메모리 해쉬맵을 통해서 인증서비스를 관리하는 것[구현체]
+        return new InMemoryUserDetailsManager(user1);
     }
 
     @Bean
     // 비밀번호 암호화를 위한 Bean
-    public PasswordEncoder passwordEncoder() {
-        // 기본적으로 사용자 비밀번호는 해독가능한 형태로 DB 에 Save 되면 안됨!
-        // 기본적으로 암호화 하거나 단방향 암호화 하는 인코더를 사용함
+    public PasswordEncoder passwordEncoder(){
+        // 기본적으로 사용자 비밀번호는 해독가능한 형태로 데이터베이스에
+        // 저장되면 안된다. 그래서 기본적으로 비밀번호를 단방향 암호화 하는
+        // 인코더를 사용한다.
         return new BCryptPasswordEncoder();
     }
 }
--- src/main/java/com/example/auth/controller/UserController.java
+++ src/main/java/com/example/auth/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.example.auth.controller;
 
+import com.example.auth.service.CustomUserDetails;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -7,7 +8,6 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,45 +17,54 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller // 로그인 페이지를 보여줄려고함
 @RequestMapping("/users")
 public class UserController {
-    // 어떻게 사용자를 관리하는지는 interface 기반으로 의존성 주입함
-    private final UserDetailsManager manager;
-    private final PasswordEncoder passwordEncoder;
 
-    // 생성자 생성[UserDetailsManager 를 사용하기 위한 것]
-    public UserController(UserDetailsManager manager, PasswordEncoder passwordEncoder) {
-        this.manager = manager;
-        this.passwordEncoder = passwordEncoder;
-    }
+    // 1. login 페이지로 온다.
+    // 2. login 페이지에 아이디 비밀번호를 입력한다.
+    // 3. 성공하면 my-profile 로 이동한다.
 
-    // 1. 로그인 페이지로 옴
-    // 2. 로그인 페이지에 아이디 비밀번호를 입력함
-    // 3. 성공하면 my-profile 로 이동함
-
-    // login page => Get[로그인 페이지를 위한 것]
+    // 로그인 페이지를 위한 GetMapping
     @GetMapping("/login")
     public String loginForm() {
         return "login-form";
     }
 
-    // login success after => Get[로그인 여부 판단]
+    // 로그인 성공 후 로그인 여부를 판단하기 위한 GetMapping
     @GetMapping("/my-profile")
     public String myProfile(
             Authentication authentication
     ) {
-        // 누가 로그인 했는지 체크할때
-        log.info(authentication.getName());
-        log.info(((User) authentication.getPrincipal()).getUsername());
-        log.info(SecurityContextHolder.getContext().getAuthentication().getName());
+//        log.info(authentication.getName());
+//        log.info(((User) authentication.getPrincipal()).getUsername());
+        CustomUserDetails userDetails
+                = (CustomUserDetails) authentication.getPrincipal();
+        log.info(userDetails.getUsername());
+        log.info(userDetails.getEmail());
+//        log.info(SecurityContextHolder.getContext().getAuthentication().getName());
         return "my-profile";
     }
 
-    // 1. 사용자가 register page 로 온다
-    // 2. 사용자가 register page 에 ID, password, password 확인을 입력
-    // 3. register page 에서 /users/register 로 POST 요청
+    // 1. 사용자가 register 페이지로 온다.
+    // 2. 사용자가 register 페이지에 ID, 비밀번호, 비밀번호 확인을 입력
+    // 3. register 페이지에서 /users/register 로 POST 요청
     // 4. UserDetailsManager 에 새로운 사용자 정보 추가
+
     @GetMapping("/register")
     public String registerForm() {
         return "register-form";
+    }
+
+    // 어떻게 사용자를 관리하는지는
+    // interface 기반으로 의존성 주입
+    private final UserDetailsManager manager;
+    private final PasswordEncoder passwordEncoder;
+
+
+    public UserController(
+            UserDetailsManager manager,
+            PasswordEncoder passwordEncoder
+    ) {
+        this.manager = manager;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @PostMapping("/register")
@@ -68,9 +77,14 @@ public class UserController {
             log.info("password match!");
             // username 중복도 확인해야 하지만,
             // 이 부분은 Service 에서 진행하는 것도 나쁘지 않아보임
-            manager.createUser(User.withUsername(username)
+//            manager.createUser(User.withUsername(username)
+//                    .password(passwordEncoder.encode(password))
+//                    .build());
+            manager.createUser(CustomUserDetails.builder()
+                    .username(username)
                     .password(passwordEncoder.encode(password))
                     .build());
+
             return "redirect:/users/login";
         }
         log.warn("password does not match...");
